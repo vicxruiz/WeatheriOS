87eb110fb0317ae39b70b9be8a9d7fbec4f377bf
Victor Ruiz <ruizdvictor@gmail.com>
2020-03-17 14:05:07 -0400

    updated storyboard ui

---
+++ WeatheriOS/ViewControllers/WeatherForeceastViewController.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherForeceastViewController.swift
+//  WeatheriOS
+//
+//  Created by Victor Ruiz on 3/17/20.
+//  Copyright © 2020 Victor Ruiz. All rights reserved.
+//
+
+import Foundation
